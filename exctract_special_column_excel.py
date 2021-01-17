7059e676593167028f7df154378f9dc7200717ff
jay08321701 <47810237+jay08321701@users.noreply.github.com>
2021-01-17 14:53:03 +0800

    Update exctract_special_column_excel.py

--- exctract_special_column_excel.py
+++ exctract_special_column_excel.py
@@ -4,12 +4,12 @@ import pandas as pd
 import os
 import xlrd
 import csv
-root = 'E:/MaoBoShi/shanxiemission'
-path = 'E:/MaoBoShi/shanxiemission/shanxi.xlsx'
-start= 1970
-for i in range(0,46):
-    file = start + i
-    i = i + 2
-    df = pd.read_excel(path, usecols=[0, 1, i])
-    df.to_csv(root+"/"+str(file)+'.csv',index=None)
+root = 'E:/MaoBoShi/shanxiemission' #设置输出的root
+path = 'E:/MaoBoShi/shanxiemission/shanxi.xlsx' #读取的目录
+start= 1970    #列开始的年份 
+for i in range(0,46): #i遍历的次数
+    file = start + i   #file为输出的excel名字
+    i = i + 2          #第三列
+    df = pd.read_excel(path, usecols=[0, 1, i]) #读取指定列的数据，但是usecols可以随意
+    df.to_csv(root+"/"+str(file)+'.csv',index=None) #输出csv
 print(df)
